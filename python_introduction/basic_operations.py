d8aa8b8c870004f7c13f04b682ba3097a9834dc8
mugai <henrymugai36@gmail.com>
2025-09-14 09:48:31 +0300

    Initial commit: added python_introduction tasks

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# basic_operations.py
+
+number1 = 10
+number2 = 5
+
+# Addition
+addition = number1 + number2
+print(f"Addition of {number1} and {number2} is {addition}")
+
+
+subtraction = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+
+
+multiplication = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# future_age_calculator.py
+
+# Prompt the user for their age
+age = int(input("How old are you? "))
+
+# Future age in 2050 (2023 + 27 years = 2050)
+future_age = age + 27
+print(f"In 2050, you will be {future_age} years old")
